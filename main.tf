89b980d70cad2ebd109bbb7a2ba619f5c5fcf6b7
MohamedA15 <mo684073@gmail.com>
2026-02-21 12:27:08 +0000

    adding helm modules

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+module "argocd" {
+  source = "./modules/argocd"
+}
+
 module "ingress_nginx" {
   source = "./modules/ingress-nginx"
+}
+
+module "cert_manager" {
+  source = "./modules/cert-manager"
 }
